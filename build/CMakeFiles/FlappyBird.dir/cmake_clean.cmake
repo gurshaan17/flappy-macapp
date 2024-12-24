5c9d40ba866a39ca8abba2450bb030145090d09f
gurshaan17 <gurshaansinghd@gmail.com>
2024-12-24 22:05:25 +0530

    first commit

---
+++ build/CMakeFiles/FlappyBird.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FlappyBird.dir/game.cpp.o"
+  "CMakeFiles/FlappyBird.dir/game.cpp.o.d"
+  "FlappyBird.app/Contents/MacOS/FlappyBird"
+  "FlappyBird.app/Contents/Resources/arial.ttf"
+  "FlappyBird.app/Contents/Resources/bird.png"
+  "FlappyBird.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FlappyBird.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
